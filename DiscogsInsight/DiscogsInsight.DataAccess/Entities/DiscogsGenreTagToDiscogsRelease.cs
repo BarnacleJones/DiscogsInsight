09a14524f0db394850b35006c0deeac65d80ca96
Banjo <benmjones@protonmail.com>
2024-03-29 12:47:22 +1300

    add new discogs genre entities

---
+++ DiscogsInsight/DiscogsInsight.DataAccess/Entities/DiscogsGenreTagToDiscogsRelease.cs
@@ -0,0 +1,14 @@
+﻿using DiscogsInsight.DataAccess.Interfaces;
+using SQLite;
+
+namespace DiscogsInsight.DataAccess.Entities
+{
+    public class DiscogsGenreTagToDiscogsRelease : IDatabaseEntity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int? DiscogsGenreTagId { get; set; }
+        public int? DiscogsArtistId { get; set; }//DiscogssArtistId on Artist - FK
+        public int? DiscogsReleaseId { get; set; }
+    }
+}
---
+++ DiscogsInsight/DiscogsInsight.DataAccess/Entities/DiscogsGenreTags.cs
@@ -0,0 +1,12 @@
+﻿using DiscogsInsight.DataAccess.Interfaces;
+using SQLite;
+
+namespace DiscogsInsight.DataAccess.Entities
+{
+    public class DiscogsGenreTags : IDatabaseEntity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string? DiscogsTag { get; set; }
+    }
+}
