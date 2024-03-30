09794df498218404cbc363d0133a6df8466a9ec0
Banjo <benmjones@protonmail.com>
2024-03-30 15:54:55 +1300

    Untagged collection page

--- DiscogsInsight/DiscogsInsight.View/Services/Collection/CollectionViewService.cs
+++ DiscogsInsight/DiscogsInsight.View/Services/Collection/CollectionViewService.cs
@@ -50,5 +50,43 @@ namespace DiscogsInsight.View.Services.Collection
                 };
             }
         }
+
+        public async Task<ViewResult<DiscogsCollectionViewModel>> GetUntaggedCollection()
+        {
+            try
+            {
+                var releases = await _collectionDataService.GetReleases();
+                //MusicBrainzId is the deciding factor as to if all API data has been retrieved
+                var releaseList = releases.Where(x => x.MusicBrainzReleaseId == null).ToList();
+
+                var artistIdsList = await _collectionDataService.GetArtistsIdsAndNames();
+
+                var viewModel = releaseList.Select(x => new ReleaseViewModel
+                {
+                    Artist = artistIdsList.Where(y => y.DiscogsArtistId == x.DiscogsArtistId).Select(x => x.Name).FirstOrDefault() ?? "No Name",
+                    DiscogsArtistId = x.DiscogsArtistId,
+                    DiscogsReleaseId = x.DiscogsReleaseId,
+                    Year = x.Year.ToString(),
+                    Title = x.Title,
+                    DateAdded = x.DateAdded
+                }).ToList();
+
+                return new ViewResult<DiscogsCollectionViewModel>
+                {
+                    Data = new DiscogsCollectionViewModel { Releases = viewModel },
+                    ErrorMessage = "",
+                    Success = true
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ViewResult<DiscogsCollectionViewModel>
+                {
+                    Data = null,
+                    ErrorMessage = ex.Message,
+                    Success = false
+                };
+            }
+        }
     }
 }
