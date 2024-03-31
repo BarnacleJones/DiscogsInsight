cb3003d05e71712b6f24e68fc1608a05a14dfb14
Banjo <benmjones@protonmail.com>
2024-03-31 16:59:24 +1300

    Tracks Insights - start with simple values

--- DiscogsInsight/DiscogsInsight.View/Services/Insights/TracksInsightsViewService.cs
+++ DiscogsInsight/DiscogsInsight.View/Services/Insights/TracksInsightsViewService.cs
@@ -17,9 +17,39 @@ namespace DiscogsInsight.View.Services.Insights
         {
             try
             {
+                var tracks = await _trackDataService.GetAllTracks();
+
+                var list = new List<string>();
+
+                var unformattedAverageTrackLength = tracks
+                                                    .Where(x => x.MusicBrainzTrackLength != null)
+                                                    .Average(x => x.MusicBrainzTrackLength);
+                var averageTrackLengthFormatted = "";
+                if (unformattedAverageTrackLength.HasValue)
+                {
+                    averageTrackLengthFormatted = $"{TimeSpan.FromMilliseconds(unformattedAverageTrackLength.Value).ToString(@"mm\:ss")}";
+                }
+
+                var releasesToTracks = tracks.GroupBy(x => x.DiscogsReleaseId).ToList();
+                
+                var tracksPerReleaseCount = new List<int>();
+                foreach ( var release in releasesToTracks ) 
+                {
+                    tracksPerReleaseCount.Add(release.Count());
+                }
+                var averageTracksPerReleaseText = Math.Round(tracksPerReleaseCount.Average(), 0, MidpointRounding.AwayFromZero).ToString();
+
+
+                var trackStatModel = new TracksInsightsStatsModel
+                {
+                    AverageTrackLength = averageTrackLengthFormatted,
+                    AverageTracksPerRelease = averageTracksPerReleaseText,
+                    HighestRatedTracks = list
+
+                };
                 return new ViewResult<TracksInsightsStatsModel>
                 {
-                    Data = null,
+                    Data = trackStatModel,
                     ErrorMessage = "",
                     Success = true
                 };
--- DiscogsInsight/DiscogsInsight.ViewModels/Insights/TracksInsightsStatsModel.cs
+++ DiscogsInsight/DiscogsInsight.ViewModels/Insights/TracksInsightsStatsModel.cs
@@ -2,5 +2,9 @@
 {
     public class TracksInsightsStatsModel
     {
+        public string AverageTrackLength { get; set; }
+        public string AverageTracksPerRelease { get; set; }
+        public List<string> HighestRatedTracks { get; set; }
+
     }
 }
