f37fae4d7d6421a90100b797fd17bd73e6ec2319
Banjo <benmjones@protonmail.com>
2024-06-08 14:04:11 +1200

    WIP fixing errors with tracks data service

--- DiscogsInsight/DiscogsInsight.DataAccess.Contract/ITracksDataService.cs
+++ DiscogsInsight/DiscogsInsight.DataAccess.Contract/ITracksDataService.cs
@@ -1,11 +1,7 @@
-﻿using DiscogsInsight.Database.Entities;
-
-namespace DiscogsInsight.DataAccess.Contract
+﻿namespace DiscogsInsight.DataAccess.Contract
 {
     public interface ITracksDataService
     {
         Task<bool> SetRatingOnTrack(int? rating, int discogsReleaseId, string title);
-        Task<List<Track>> GetAllTracks();
-        Task<List<Track>> GetTracksForRelease(int? discogsReleaseId);
     }
 }
